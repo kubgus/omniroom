e48cd55c1f23e75a0b06a8682bce81194f639c92
kubgus <ja.gustafik@outlook.sk>
2023-10-20 18:07:06 +0200

    Working demo!!! 🚀

--- client/public/js/main.js
+++ client/public/js/main.js
@@ -1,1 +1,48 @@
 const socket = io();
+
+const canvas = document.getElementById("game");
+const ctx = canvas.getContext("2d");
+
+let x = 0;
+let y = 0;
+let r = Math.random() * 255;
+let g = Math.random() * 255;
+let b = Math.random() * 255;
+
+document.addEventListener("keypress", (event) => {
+    const speed = 10;
+    if (event.key === "w") {
+        y -= speed;
+    }
+    if (event.key === "a") {
+        x -= speed;
+    }
+    if (event.key === "s") {
+        y += speed;
+    }
+    if (event.key === "d") {
+        x += speed;
+    }
+});
+
+socket.on("serverUpdate", (connections) => {
+    ctx.canvas.width = window.innerWidth;
+    ctx.canvas.height = window.innerHeight;
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    for (const id in connections) {
+        const connection = connections[id];
+        ctx.fillStyle = `rgb(${connection.r}, ${connection.g}, ${connection.b})`;
+        ctx.fillRect(connection.x, connection.y, 50, 50);
+    }
+});
+
+const SendToServer = () => {
+    socket.emit("clientUpdate", { x, y, r, g, b })
+}
+
+const GameLoop = () => {
+    SendToServer();
+    requestAnimationFrame(GameLoop);
+}
+// Initial call to the game loop
+GameLoop();
--- server.ts
+++ server.ts
@@ -13,8 +13,24 @@ const sio = new Server(exs);
 app.set("views", path.join(__dirname, "/client/views"));
 app.use(express.static(__dirname + '/client/public'));
 
+let connections: any = {};
+
 sio.on("connection", (io) => {
     console.log(`🤖 Session ${io.id} started!`);
+    connections[io.id] = {
+        x: 0, y: 0,
+        r: 255, g: 0, b: 0
+    };
+
+    io.on("clientUpdate", (data) => {
+        connections[io.id] = data;
+        io.emit("serverUpdate", connections);
+    });
+
+    io.on("disconnect", () => {
+        console.log(`❌ Session ${io.id} ended!`);
+        delete connections[io.id];
+    });
 });
 
 app.get("/", (req, res) => { res.render("index.ejs") });
